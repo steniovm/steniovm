5603a5c7539465160515c500204216734e53e26d
steniovm <steniovm@gmail.com>
2022-12-29 01:52:02 -0300

    add APOD aplication

--- APODnasaBR/scripts.js
+++ APODnasaBR/scripts.js
@@ -5,7 +5,22 @@ const imagefigure = document.getElementById('imagefigure');
 const videoframe = document.getElementById('videoframe');
 const explanationimage = document.getElementById('explanationimage');
 const lowdownload = document.getElementById('lowdownload');
-const highdownload =document.getElementById('highdownload');
+const highdownload = document.getElementById('highdownload');
+const mtitleimage = document.getElementById('mtitleimage');
+const mcopyrightimage = document.getElementById('mcopyrightimage');
+const mdateimage = document.getElementById('mdateimage');
+const mimagefigure = document.getElementById('mimagefigure');
+const mvideoframe = document.getElementById('mvideoframe');
+const mexplanationimage = document.getElementById('mexplanationimage');
+const mlowdownload = document.getElementById('mlowdownload');
+const mhighdownload = document.getElementById('mhighdownload');
+const datestart = document.getElementById('datestart');
+const dateend = document.getElementById('dateend');
+const btsearch = document.getElementById('btsearch');
+const mosaic = document.getElementById('mosaic');
+const modalmosaic = document.getElementById('modalmosaic');
+const nrandom = document.getElementById('nrandom');
+const btrand = document.getElementById('btrand');
 const urlbase = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&thumbs=true&"
 const dateobject = new Date();
 let dataday;
@@ -19,7 +34,8 @@ function formatDate(date) {
   }
   return `${map.aaaa}-${map.mm}-${map.dd}`
 }
-function updateelements(data){
+
+function updateelementsod(data){
   titleimage.innerHTML = data.title;
   copyrightimage.innerHTML = data.copyright;
   if (data.media_type == "image") {
@@ -37,7 +53,47 @@ function updateelements(data){
   highdownload.href = data.hdurl;
 }
 
-function consultapi(parans){
+function updateelementsld(data){
+  mosaic.innerHTML = '';
+  data.forEach((element, index) => {
+    let img = document.createElement('img');
+    img.classList.add("mosaicimg");
+    if (element.media_type == "image") {
+      img.src = element.url;
+    }
+    if (element.media_type == "video") {
+      img.src = element.thumbnail_url;
+    }
+    img.addEventListener('click',function(){
+      showmodal(index);
+      window.scrollTo(0, 0);
+    })
+    mosaic.append(img);
+  });
+}
+
+function showmodal(index){
+  console.log(dataday[index]);
+  modalmosaic.style.display = "flex";
+  mtitleimage.innerHTML = dataday[index].title;
+  mcopyrightimage.innerHTML = dataday[index].copyright;
+  mdateimage.innerHTML = dataday[index].date;
+  if (dataday[index].media_type == "image") {
+    mvideoframe.style.display = "none"
+    mimagefigure.src = dataday[index].url;
+  }
+  if (dataday[index].media_type == "video") {
+    mvideoframe.style.display = "unset"
+    mimagefigure.src = dataday[index].thumbnail_url;
+    mvideoframe.src = dataday[index].url;
+  }
+  mimagefigure.alt = dataday[index].title;
+  mexplanationimage.innerHTML = dataday[index].explanation;
+  mlowdownload.href = dataday[index].url;
+  mhighdownload.href = dataday[index].hdurl;
+}
+
+function consultapi(parans,cb){
   console.log(urlbase+parans);
   fetch(urlbase+parans).then(function(response){
     if (response.status !== 200) {
@@ -46,7 +102,7 @@ function consultapi(parans){
     }
     response.json().then(function(data) {
       dataday = data;
-      updateelements(dataday);
+      cb(dataday);
       console.log(JSON.stringify(data, null, " "))
       console.log(dataday);
     })
@@ -56,82 +112,35 @@ function consultapi(parans){
 }
 
 datein.value = formatDate(dateobject);
-consultapi("date="+datein.value);
+consultapi("date="+datein.value, updateelementsod);
+
 datein.addEventListener('change',function(){
-  consultapi("date="+datein.value);
+  consultapi("date="+datein.value, updateelementsod);
   console.log(datein.value);
 });
 
-fetchTranslation("translate this text");
-
-function fetchTranslation(text){
-  let headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', 'application/json');
-  headers.append('Origin','http://localhost');
-
-  fetch(`https://translate.google.com/?sl=en&tl=pt&text=${text}&op=translate`, {
-    mode: 'no-cors',
-    credentials: 'include',
-    method: 'POST',
-    headers: headers
-  }).then(function(response){
-    console.log(response);
-    response.json().then(function(data){
-      console.log(data);
-    });
-  });
-}
-
-
-
-/*
-//variaveis globais
-let inputgrups = document.getElementById('inputgrups');
-let outputgrups = document.getElementById('outputgrups');
-let ngrups = document.getElementById('ngrups');
-let baction = document.getElementById('baction');
-
-//funções
-// Randomize array in-place using Durstenfeld shuffle algorithm
-function shuffleArray(_array) {
-  let array = _array;
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-  }
-  return array;
-}
-
-function goRandom(){
-  const nunberGrups = ngrups.value;
-  const listItens = inputgrups.value.split("\n");
-  console.log(listItens);
-  const shuffledArray = shuffleArray(listItens);
-  let i = 1;
-  let j = 1;
-  let result = `====== grupo ${j} ======\n`;
-  shuffledArray.forEach( (item) => {
-    if (i < nunberGrups) {
-      result += item + "\n";
-      i++;
-    } else {
-      result += item + "\n";
-      i = 1;
-      j++;
-      result += `====== grupo ${j} ======\n`;
+btsearch.addEventListener('click', function(){
+  if(datestart.value && dateend.value){
+    if(datestart.value < dateend.value){
+      consultapi("start_date="+datestart.value+"&end_date="+dateend.value, updateelementsld);
+      console.log(datestart.value+" - "+dateend.value);
+    }else{
+      alert("A data de termino deve ser maior que a data de inicio!")
     }
-  });
-  outputgrups.value=result;
-  console.log(shuffledArray);
-  console.log(result);
-}
+  }else{
+    alert("As datas de inicio e termino da busca devem estar preenchidas!")
+  }
+});
 
+btrand.addEventListener('click', function(){
+  if(nrandom.value){
+      consultapi("count="+nrandom.value, updateelementsld);
+      console.log(nrandom.value);
+  }else{
+    alert("Insira um numero de imagens!")
+  }
+});
 
-
-//eventos
-baction.addEventListener('click', goRandom);
-
-*/
+modalmosaic.addEventListener('click', function(){
+  modalmosaic.style.display = "none";
+});
