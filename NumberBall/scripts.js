5d66a667c69b90c2b28f45933cb823659860a84b
steniovm <steniovm@gmail.com>
2023-01-10 02:00:43 -0300

    colisoes futebol

--- NumberBall/scripts.js
+++ NumberBall/scripts.js
@@ -1,4 +1,5 @@
 const LIMITS = {top:0,left:0,down:92,right:88,downb:96,rightb:94};
+const VELOCOM = 0.5;
 const gamerconfig = document.getElementById('gamerconfig');
 const alga = document.getElementsByName('alga');
 const dire = document.getElementsByName('dire');
@@ -13,6 +14,7 @@ const ballgame = document.querySelector('.ballgame');
 const cabec = document.querySelectorAll('.cabec');
 const algar = document.querySelectorAll('.algar');
 const playactiv = document.getElementById('playactiv');
+const arrow = document.getElementById('arrow');
 let positions = {
     ball:{x:0,y:0},
     cabec:[{x:0,y:0}],
@@ -35,6 +37,7 @@ let direcbt={up:"",left:"",down:"",right:""};
 let finterval = undefined;
 
 //funções
+//carrega as configurações
 function upconfig(){
     alga.forEach(function(item,index){
         if (item.checked){
@@ -52,12 +55,14 @@ function upconfig(){
     config.vefec = Vefect.value;
     if (printok) console.log(config);
 }
+//salva configurações em cookie
 function saveconf(){
     const d = new Date();
     d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
     document.cookie = 'numberball' + "=" + JSON.stringify(config) + ";" + expires + ";path=/";
 }
+//renderiza os jogadores nas suas posições atuais em campo
 function showplayers(){
     ballgame.style.top = positions.ball.y+'%';
     ballgame.style.left = positions.ball.x+'%';
@@ -67,7 +72,12 @@ function showplayers(){
         algar[i].style.top = positions.algar[i].y+'%';
         algar[i].style.left = positions.algar[i].x+'%';
     }
+    if (playac){
+        arrow.style.top = (positions.algar[playac].y-2)+'%';
+        arrow.style.left = (positions.algar[playac].x-2)+'%';
+    }
 }
+//sorteia a posição atual de cada jogador em campo
 function positioninit(){
     positions.ball = {x:47,y:48};
     positions.cabec[0] = {x:44,y:2};
@@ -83,11 +93,13 @@ function positioninit(){
     showplayers();
     if (printok) console.log(positions);
 }
+//renderiza os numeros dos jogadores conforme escolha dos algarismos
 function shownambers(){
     for (let i=0;i<10;i++){
         algar[i].src="./assets/"+alga[config.valga].value+i+".svg";
     }
 }
+//configura botões direcionais conforme configurado
 function direcbtdef(){
     if (config.vdire==0){
         direcbt.up = "ArrowUp";
@@ -101,19 +113,47 @@ function direcbtdef(){
         direcbt.right = "d";
     }
 }
+//executa a partida dentro do tempo
 async function playing(){
     if (gameplay){
         let dx;
         let dy;
         finterval = setInterval(function(){
+            //detecta colisão bola parede
             if (positions.ball.x < LIMITS.left || positions.ball.x > LIMITS.rightb){
                 bollvec.dx *= -1;
             }
             if (positions.ball.y < LIMITS.top || positions.ball.y > LIMITS.downb){
                 bollvec.dy *= -1;
             }
+            //detecta colisão bola jogador
+            for (let i=0;i<10;i++){
+                let ddx = (positions.ball.x)-(positions.cabec[i].x)-4;
+                let ddy = (positions.ball.y)-(positions.cabec[i].y)-4;
+                let dd = Math.sqrt(Math.pow(ddx,2)+Math.pow(ddy,2));
+                if (dd<=8){
+                    bollvec.dy = VELOCOM;
+                    if (bollvec.dx >=0) {bollvec.dx = VELOCOM;}
+                    else {bollvec.dx = -VELOCOM;}
+                }
+                ddx = (positions.ball.x)-(positions.algar[i].x)-4;
+                ddy = (positions.ball.y)-(positions.algar[i].y)-4;
+                dd = Math.sqrt(Math.pow(ddx,2)+Math.pow(ddy,2));
+                if (dd<=8){
+                    if (i!=playac){
+                        bollvec.dy = -VELOCOM;
+                        if (bollvec.dx >=0) {bollvec.dx = VELOCOM;}
+                        else {bollvec.dx = -VELOCOM;}
+                    }else{
+                        bollvec.dy = -(2*VELOCOM*Math.cos(direc*(Math.PI/180)));
+                        bollvec.dx = (2*VELOCOM*Math.sin(direc*(Math.PI/180)));
+                    }
+                }
+            }
+            //move a bola
             positions.ball.x += bollvec.dx;
             positions.ball.y += bollvec.dy;
+            //move jogadores aleatóriamente (movimento browniano) exceto jogador selecionado
             for (let i=0; i<10; i++){
                dx = Math.random()*2-1;
                dy = Math.random()*2-1;
@@ -132,19 +172,23 @@ async function playing(){
                 }
                } 
             }
+            //atualiza posição dos jogadores
             showplayers();
-        },100*(1.1-config.veloc));
+        },100*(1.1-config.veloc));//timer conforme velocidade configurada
     }
 }
 
 //script inicial
+//resgata configurações salvas em cookie
 if (document.cookie.indexOf('numberball')>=0){
     config = JSON.parse(document.cookie.split("; ").find((row) => row.startsWith('numberball='))?.split("=")[1])
     upconfig();
 }
+//inicializa posição dos jogadores em campo
 positioninit();
 
 //eventos
+//botão iniciar
 Iniciar.addEventListener('click',function(){
     upconfig();
     shownambers();
@@ -153,23 +197,25 @@ Iniciar.addEventListener('click',function(){
     gameplay = true;
     playing();
 });
-
+//botão salvar configurações
 Saveps.addEventListener('click',function(){
     upconfig();
     saveconf()
 });
-
+//botão reiniciar jogo volta para tela de configurações
 bRestart.addEventListener('click',function(){
     gameplay = false;
     gamerconfig.style.display = 'flex';
     clearInterval(finterval);
-})
-
+});
+//pressionar teclas
 document.addEventListener('keydown',function(e){
+    //ação somente se jogo iniciado
     if (gameplay){
         const key = e.key;
         console.log(key)
         switch (key){
+            //se digita um numero seleciona um jogador em campo
             case "1":
             case "2":
             case "3":
@@ -184,6 +230,7 @@ document.addEventListener('keydown',function(e){
                 playactiv.innerHTML = key;
                 algar[playac].classList.toggle('enf');
             break;
+            //se direcional move o jogador selecionado
             case direcbt.left:
             case direcbt.left.toUpperCase():
                 if (playac && positions.algar[playac].x>LIMITS.left){
@@ -208,354 +255,21 @@ document.addEventListener('keydown',function(e){
                     positions.algar[playac].y += 1;
                 }
             break;
+            //se < ou > rotaciona o vetor do chute
             case "<":
             case ",":
+                if (direc > -90) {
+                    direc -= 2;
+                    arrow.style.transform = "rotate("+direc+"deg)";
+                }
+            break;
             case ">":
             case ".":
-                playactiv.innerHTML = key;
+                if (direc < 90) {
+                    direc += 2;
+                    arrow.style.transform = "rotate("+direc+"deg)";
+                }
             break;
         }
-        console.log(e.key);
     }
-})
-/*
-const bodypage = document.getElementById('bodypage');
-const textprint = document.getElementById('text');
-const nMordidas = document.getElementById('NMordidas');
-const nPresas = document.getElementById('NPresas');
-const textlog = document.getElementById('textlog');
-const mycanvas = document.querySelector('canvas');
-const gamerconfig = document.getElementById('gamerconfig');
-const gamerplay = document.getElementById('gamerplay');
-const biniciar = document.getElementById('Iniciar')
-const nmord = document.getElementById('NM');
-const npres = document.getElementById('NP');
-const iraio = document.getElementById('IRaio');
-const ltime = document.getElementById('ltime');
-const ltimemax = document.getElementById('ltimemax');
-const bRestart = document.getElementById('bRestart');
-const IMax = document.getElementById('IMax');
-const Itime = document.getElementById('Itime');
-const IChance = document.getElementById('IChance');
-const colorrgb = document.querySelectorAll('.icolor');
-const mostAmb = document.getElementById('mostAmb');
-const mostBol = document.getElementById('mostBol');
-const Vmusic = document.getElementById('Vmusic');
-const Vefect = document.getElementById('Vefect');
-const efectsong = new Audio("./assets/drop.wav");
-const oversong = new Audio("./assets/defeat-sound.mp3");
-const music = new Audio("./assets/jigsaw-puzzle-background.mp3");
-music.volume = Vmusic.value;
-efectsong.volume = Vefect.value;
-oversong.volume = Vefect.value;
-
-let c = mycanvas.getContext('2d');
-let paused = true;
-let nbol = npres.value;
-let rbol = iraio.value;
-c.width = mycanvas.offsetWidth;
-c.height = mycanvas.offsetHeight;
-let bolinhas = [];
-let energy = nmord.value;
-let timegame = 0;
-let timegamemax = 0;
-let repchance = (IChance.value)/100;
-let maxbols = Number.parseInt(IMax.value);
-let reptime = Itime.value;
-let contpresas = 0;
-mostAmb.style.height = (2*iraio.value + 4)+'px';
-mostAmb.style.width = (2*iraio.value + 4)+'px';
-mostBol.style.height = (2*iraio.value)+'px';
-mostBol.style.width = (2*iraio.value)+'px';
-
-function setCookie() {
-  const d = new Date();
-  d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = 'boldarwintime' + "=" + timegamemax + ";" + expires + ";path=/";
-}
-
-if (document.cookie.indexOf('boldarwintime')>=0){
-    timegamemax = JSON.parse(document.cookie.split("; ").find((row) => row.startsWith('boldarwintime='))?.split("=")[1])
-    ltimemax.innerHTML = timegamemax;
-}else{
-    setCookie();
-}
-
-function changeColor(){
-  let red = Math.floor(Math.random()*255);
-  let green = Math.floor(Math.random()*255);
-  let blue = Math.floor(Math.random()*255);
-  mycanvas.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  mostAmb.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  mostBol.style.backgroundColor = 'rgb(' + (255-red) + ',' + (255-green) + ',' + (255-blue) + ')';
-  colorrgb[0].value = red;
-  colorrgb[0].style.accentColor = "rgb("+red+",0,0)";
-  colorrgb[1].value = green;
-  colorrgb[1].style.accentColor = "rgb(0,"+green+",0)";
-  colorrgb[2].value = blue;
-  colorrgb[2].style.accentColor = "rgb(0,0,"+blue+")";
-}
-
-function changeNewColor(){
-  mycanvas.style.backgroundColor = 'rgb(' + colorrgb[0].value + ',' + colorrgb[1].value + ',' + colorrgb[2].value + ')';
-}
-
-function bolinha(x,y,raio,r,g,b){
-    this.x = x + Math.floor(Math.random()*10 - 5);
-    this.y = y + Math.floor(Math.random()*10 - 5);
-    this.ra = raio;
-    this.r = r + Math.floor(Math.random()*10 - 5);
-    this.g = g + Math.floor(Math.random()*10 - 5);
-    this.b = b + Math.floor(Math.random()*10 - 5);
-    this.dx = Math.random() - 0.5;
-    this.dy = Math.random() - 0.5;
-    this.mostrar = function(){
-        c.beginPath();
-        c.arc(this.x, this.y, this.ra, 0, 2*Math.PI);
-        c.fillStyle=('rgb('+this.r+','+this.g+','+this.b+')');
-        c.fill();
-    }
-    this.mover = function(){
-        if(this.x+this.dx>0 && this.x+this.dx<c.width){
-            this.x = this.x + this.dx;
-        }else{
-            this.dx = -this.dx;
-            this.x = this.x + this.dx;
-        }
-        if(this.y+this.dy>0 && this.y+this.dy<c.height){
-            this.y = this.y+this.dy;
-        }else{
-            this.dy = -this.dy;
-            this.y = this.y+this.dy;
-        }
-        this.mostrar();
-    }
-    
-}
-
-function vetorbol(nb){
-    let bolinhas = [];
-    for(let i=0; i<nb; i++){
-        let x = Math.floor(Math.random()*c.width);
-        let y = Math.floor(Math.random()*c.height);
-        let r = Math.floor(Math.random()*255);
-        let g = Math.floor(Math.random()*255);
-        let b = Math.floor(Math.random()*255);
-        bolinhas.push(new bolinha(x,y,rbol,r,g,b));
-    }
-    nPresas.innerHTML = nb;
-    return bolinhas;
-}
-
-function inicio(){
-    if (paused) return;
-    bolinhas = vetorbol(nbol);
-    nPresas.style.width = ((1 - nbol/maxbols)*100)+'%';
-    for(i=0;i<nbol;i++){
-        bolinhas[i].mostrar();
-    }
-    mostrarbol();
-}
-
-function mostrarbol(){
-    if (paused) return;
-    c.clearRect(0,0,innerWidth,innerHeight);
-    requestAnimationFrame(mostrarbol);
-    for(i=0;i<nbol;i++){
-        bolinhas[i].mover();
-    }
-}
-
-function pinicio(){
-    nbol = npres.value;
-    rbol = iraio.value;
-    energy = nmord.value;
-    nMordidas.innerHTML = energy;
-    nMordidas.style.width = (100-energy)+'%';
-    maxbols = Number.parseInt(IMax.value);
-    reptime = Itime.value;
-    repchance = (IChance.value)/100;
-    contpresas = 0;
-    paused = false;
-    music.play();
-    music.loop = true;
-    textlog.innerHTML = `Dados do jogo
-Energia inicial: ${energy} mordidas
-Raio das bolinhas: ${rbol} pixels
-População inicial: ${nbol} bolinhas
-População maxima: ${maxbols} bolinhas
-Periodo de reprodução: ${reptime} segundos
-Chance de reprodução: ${repchance*100}%
-Ambiente: ${mycanvas.style.backgroundColor}`;
-}
-
-function clicar(e){
-    if (paused) return;
-    let mx = e.clientX;
-    let my = e.clientY;
-    for(i=(nbol-1);i>=0;i--){
-        let dx = mx-bolinhas[i].x;
-        if (dx < 0 ) dx = - dx;
-        let dy = my-bolinhas[i].y;
-        if (dy < 0 ) dy = - dy;
-        if((dx<(bolinhas[i].ra)) && (dy<(bolinhas[i].ra))){
-            efectsong.play();
-            textlog.innerHTML += "\nAcertou: ("+mx+","+my+")";
-            textlog.scrollTop = textlog.scrollHeight;
-            contpresas++;
-            if (energy<100) energy++;
-            nMordidas.innerHTML = energy;
-            nMordidas.style.width = (100-energy)+'%';
-            bolinhas.splice(i,1);
-            nbol = bolinhas.length
-            nPresas.innerHTML = nbol;
-            nPresas.style.width = ((1 - nbol/maxbols)*100)+'%';
-            return;
-        }else{
-            if (i == 0) {
-                textlog.innerHTML += "\nErrou: ("+mx+","+my+")";
-                textlog.scrollTop = textlog.scrollHeight;
-                if (energy) energy--;
-                nMordidas.innerHTML = energy;
-                nMordidas.style.width = (100-energy)+'%';
-            }
-        }
-    }
-}
-function newbirth(n){
-    if (paused) return;
-    let bolmae = bolinhas[n];
-    let x = bolmae.x + Math.floor(Math.random()*10 - 5);;
-    let y = bolmae.y + Math.floor(Math.random()*10 - 5);;
-    let r = bolmae.r + Math.floor(Math.random()*50 - 25);
-    if(r > 255) r = 254;
-    if(r < 0) r = 1;
-    let g = bolmae.g + Math.floor(Math.random()*50 - 25);
-    if(g > 255) g = 254;
-    if(g < 0) g = 1;
-    let b = bolmae.b + Math.floor(Math.random()*50 - 25);
-    if(b > 255) b = 254;
-    if(b < 0) b = 1;
-    bolinhas.push(new bolinha(x,y,rbol,r,g,b));
-    nbol = bolinhas.length
-    nPresas.innerHTML = nbol;
-    nPresas.style.width = ((1 - nbol/maxbols)*100)+'%';
-}
-
-async function pastime(){
-    if (paused) return;
-    let timeciclo = 0;
-    let interval = setInterval(function(){
-        timegame++;
-        timeciclo++;
-        ltime.innerHTML = timegame;
-        if (nbol>=maxbols){
-            clearInterval(interval);
-            textlog.innerHTML += "\nAs bolinhas dominaram o mundo e mataram os predadores!!!";
-            textlog.innerHTML += "\nVoce sobreviveu por "+timegame+" segundos e abateu "+contpresas+" presas!";
-            textlog.scrollTop = textlog.scrollHeight;
-            music.pause();
-            oversong.play();
-            if (timegame > timegamemax){
-                timegamemax = timegame;
-                ltimemax.innerHTML = timegamemax;
-                setCookie();
-            }
-            paused = true;
-        }else if (nbol<=0){
-            clearInterval(interval);
-            textlog.innerHTML += "\nAs bolinhas foram extintas e os predadores morreram de fome!!!";
-            textlog.innerHTML += "\nVoce sobreviveu por "+timegame+" segundos e abateu "+contpresas+" presas!";
-            textlog.scrollTop = textlog.scrollHeight;
-            music.pause();
-            oversong.play();
-            if (timegame > timegamemax){
-                timegamemax = timegame;
-                ltimemax.innerHTML = timegamemax;
-                setCookie();
-            }
-            paused = true;
-        }else if (energy <= 0){
-            clearInterval(interval);
-            textlog.innerHTML += "\nSua energia vital acabou e voce desfaleceu!!!";
-            textlog.innerHTML += "\nVoce sobreviveu por "+timegame+" segundos e abateu "+contpresas+" presas!";
-            textlog.scrollTop = textlog.scrollHeight;
-            music.pause();
-            oversong.play();
-            if (timegame > timegamemax){
-                timegamemax = timegame;
-                setCookie();
-            }
-            paused = true;
-        }
-        let max = bolinhas.length;
-        if (timeciclo==reptime){
-            if (energy > 0) {
-                energy--;
-                nMordidas.innerHTML = energy;
-                nMordidas.style.width = (100-energy)+'%';
-            }
-            let s = 0
-            for(let i=0;i<max;i++){
-            s = Math.random();
-            if (s < repchance) {
-                newbirth(i);
-            }
-            }
-            timeciclo = 0;
-        }
-    },1000); 
-}
-
-biniciar.addEventListener("click", function(){
-    gamerconfig.style.display = "none";
-    gamerplay.style.display = "flex";
-    pinicio();
-    inicio();
-    mostrarbol();
-    pastime();
-    changeNewColor();
 });
-bRestart.addEventListener("click", function(){
-    gamerconfig.style.display = "flex";
-    music.loop = false;
-    bolinhas = [];
-    timegame = 0;
-    paused = true;
-})
-
-colorrgb[0].addEventListener('change', function(){
-    colorrgb[0].style.accentColor = "rgb("+colorrgb[0].value+",0,0)";
-    mostAmb.style.backgroundColor = "rgb("+colorrgb[0].value+","+colorrgb[1].value+","+colorrgb[2].value+")";
-})
-colorrgb[1].addEventListener('change', function(){
-    colorrgb[1].style.accentColor = "rgb(0,"+colorrgb[1].value+",0)";
-    mostAmb.style.backgroundColor = "rgb("+colorrgb[0].value+","+colorrgb[1].value+","+colorrgb[2].value+")";
-})
-colorrgb[2].addEventListener('change', function(){
-    colorrgb[2].style.accentColor = "rgb(0,0,"+colorrgb[2].value+")";
-    mostAmb.style.backgroundColor = "rgb("+colorrgb[0].value+","+colorrgb[1].value+","+colorrgb[2].value+")";
-})
-iraio.addEventListener('change', function(){
-    mostAmb.style.height = (2*iraio.value + 4)+'px';
-    mostAmb.style.width = (2*iraio.value + 4)+'px';
-    mostBol.style.height = (2*iraio.value)+'px';
-    mostBol.style.width = (2*iraio.value)+'px';
-})
-Vmusic.addEventListener('change', function(){
-    music.volume = Vmusic.value;
-    music.play();
-    setTimeout(function (){
-        music.pause();
-    },500);
-});
-Vefect.addEventListener('change', function(){
-    efectsong.volume = Vefect.value;
-    oversong.volume = Vefect.value;
-    efectsong.play();
-});
-
-changeColor();
-mycanvas.addEventListener('mouseup',clicar,true);
-*/
